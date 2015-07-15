482f4211c49efbb14783d5e0fff1c4fba881bc58
jeanette fairless & jocelyn alsdorf <student@epicodus.com>
2015-07-15 09:52:31 -0700

    setup framework with devise

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+
+ #
+ # factory :user do
+ #  	name('jocelyn alsdorf')
+ #    sequence(:email) {|n| "test#{n}@example.com" }
+ #    password('12345678')
+ #
+ #  end
+
+
+
+end
